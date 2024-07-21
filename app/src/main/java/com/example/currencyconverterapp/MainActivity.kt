297a015afe0ee0058132de16c65fa9bf18a4b066
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-21 15:55:26 +0700

    declare and initial widget

--- app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.currencyconverterapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputUsd: EditText
+    private lateinit var outputVnd: TextView
+    private lateinit var makeConvertBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        inputUsd = findViewById(R.id.inputUsd)
+        outputVnd = findViewById(R.id.outputVnd)
+        makeConvertBtn = findViewById(R.id.makeConvertBtn)
     }
 }
